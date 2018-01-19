2a753bd13bb0a9929f5c26e7ab5a60dc06669127
Peter Brändel <peter.braendel@xing.com>
2018-01-19 17:31:46 +0100

    Renaming and formatting

--- lib/poloniex_api/public.ex
+++ lib/poloniex_api/public.ex
@@ -2,7 +2,7 @@ defmodule PoloniexApi.Public do
   @moduledoc """
   # Implementation of Poloniex' Public Api
   ## Currently Implemented Methods
-    + returnTicker
+    + returnCurrency
     + return24hVolume
     + returnOrderBook
     + returnTradeHistory
@@ -13,85 +13,85 @@ defmodule PoloniexApi.Public do
 
   @default_timestamp_offset -600
 
-  def ticker do
-    fetch_ticker()
+  def currency do
+    fetch_currency()
   end
 
-  @spec ticker(binary, binary) :: map
-  def ticker(source_ticker, target_ticker) do
-    combined_ticker = combine_tickers(source_ticker, target_ticker)
+  @spec currency(binary, binary) :: map
+  def currency(source_currency, target_currency) do
+    combined_currency = combine_currencies(source_currency, target_currency)
 
-    case fetch_ticker() do
-      {:ok, currencies} -> find_ticker(currencies, combined_ticker)
+    case fetch_currency() do
+      {:ok, currencies} -> find_currency(currencies, combined_currency)
       error -> error
     end
   end
 
   def volume24, do: fetch_24_volume()
 
-  def volume24(source_ticker, target_ticker) do
-    combined_ticker = combine_tickers(source_ticker, target_ticker)
+  def volume24(source_currency, target_currency) do
+    combined_currency = combine_currencies(source_currency, target_currency)
 
     case fetch_24_volume() do
-      {:ok, currencies} -> find_ticker(currencies, combined_ticker)
+      {:ok, currencies} -> find_currency(currencies, combined_currency)
       error -> error
     end
   end
 
   def order_book, do: fetch_order_book("all")
 
-  def order_book(source_ticker, target_ticker) do
-    combined_ticker = combine_tickers(source_ticker, target_ticker)
+  def order_book(source_currency, target_currency) do
+    combined_currency = combine_currencies(source_currency, target_currency)
 
-    fetch_order_book(combined_ticker)
+    fetch_order_book(combined_currency)
   end
 
-  def trade_history(source_ticker, target_ticker) do
-    combined_ticker = combine_tickers(source_ticker, target_ticker)
+  def trade_history(source_currency, target_currency) do
+    combined_currency = combine_currencies(source_currency, target_currency)
     start_timestamp = unix_timestamp(@default_timestamp_offset)
     end_timestamp = unix_timestamp()
 
-    fetch_trade_history(combined_ticker, start_timestamp, end_timestamp)
+    fetch_trade_history(combined_currency, start_timestamp, end_timestamp)
   end
 
-  def trade_history(source_ticker, target_ticker, start_timestamp, end_timestamp) do
-    combined_ticker = combine_tickers(source_ticker, target_ticker)
-    fetch_trade_history(combined_ticker, start_timestamp, end_timestamp)
+  def trade_history(source_currency, target_currency, start_timestamp, end_timestamp) do
+    combined_currency = combine_currencies(source_currency, target_currency)
+    fetch_trade_history(combined_currency, start_timestamp, end_timestamp)
   end
 
-  def chart_data(source_ticker, target_ticker, period \\ 300)
+  def chart_data(source_currency, target_currency, period \\ 300)
       when period in [300, 900, 1800, 7200, 14400, 86400] do
-    combined_ticker = combine_tickers(source_ticker, target_ticker)
+    combined_currency = combine_currencies(source_currency, target_currency)
     start_timestamp = unix_timestamp(@default_timestamp_offset)
     end_timestamp = unix_timestamp()
 
-    fetch_chart_data(combined_ticker, start_timestamp, end_timestamp, period)
+    fetch_chart_data(combined_currency, start_timestamp, end_timestamp, period)
   end
 
-  def chart_data(source_ticker, target_ticker, start_timestamp, end_timestamp, period \\ 300)
+  def chart_data(source_currency, target_currency, start_timestamp, end_timestamp, period \\ 300)
       when period in [300, 900, 1800, 7200, 14400, 86400] do
-    combined_ticker = combine_tickers(source_ticker, target_ticker)
+    combined_currency = combine_currencies(source_currency, target_currency)
 
-    fetch_chart_data(combined_ticker, start_timestamp, end_timestamp, period)
+    fetch_chart_data(combined_currency, start_timestamp, end_timestamp, period)
   end
 
   def currencies do
     fetch_currencies()
   end
 
-  def currencies(ticker) do
+  def currencies(currency) do
     case fetch_currencies() do
-      {:ok, currencies} -> find_ticker(currencies, ticker)
+      {:ok, currencies} -> find_currency(currencies, currency)
       error -> error
     end
   end
 
-  def loan_orders(ticker) do
-    get([{"command", "returnLoanOrders"}, {"currency", ticker}])
+  def loan_orders(currency) do
+    get([{"command", "returnLoanOrders"}, {"currency", currency}])
   end
 
-  defp find_ticker(collection, ticker) do
-    case collection |> Enum.find(fn {curr, _data} -> curr == ticker end) do
+  defp find_currency(collection, currency) do
+    case collection |> Enum.find(fn {curr, _data} -> curr == currency end) do
       nil -> {:error, "Currency not found"}
       success -> {:ok, success}
     end
@@ -101,31 +101,31 @@ defmodule PoloniexApi.Public do
     get([{"command", "returnCurrencies"}])
   end
 
-  defp fetch_chart_data(ticker, start_timestamp, end_timestamp, period) do
+  defp fetch_chart_data(currency, start_timestamp, end_timestamp, period) do
     get([
       {"command", "returnChartData"},
-      {"currencyPair", ticker},
+      {"currencyPair", currency},
       {"start", start_timestamp},
       {"end", end_timestamp},
       {"period", period}
     ])
   end
 
-  defp fetch_trade_history(ticker, start_timestamp, end_timestamp) do
+  defp fetch_trade_history(currency, start_timestamp, end_timestamp) do
     get([
       {"command", "returnTradeHistory"},
-      {"currencyPair", ticker},
+      {"currencyPair", currency},
       {"start", start_timestamp},
       {"end", end_timestamp}
     ])
   end
 
-  defp fetch_order_book(ticker) do
-    get([{"command", "returnOrderBook"}, {"currencyPair", ticker}])
+  defp fetch_order_book(currency) do
+    get([{"command", "returnOrderBook"}, {"currencyPair", currency}])
   end
 
-  defp fetch_ticker do
-    get([{"command", "returnTicker"}])
+  defp fetch_currency do
+    get([{"command", "returnCurrency"}])
   end
 
   defp fetch_24_volume do
@@ -155,7 +155,7 @@ defmodule PoloniexApi.Public do
     |> round
   end
 
-  defp combine_tickers(source_ticker, target_ticker) do
-    String.upcase(source_ticker) <> "_" <> String.upcase(target_ticker)
+  defp combine_currencies(source_currency, target_currency) do
+    String.upcase(source_currency) <> "_" <> String.upcase(target_currency)
   end
 end
